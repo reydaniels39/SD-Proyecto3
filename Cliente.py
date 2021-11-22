acd0ab6d88a5670cc32676f2d8a9b61f128df716
Daniel Aros Ramírez <danielaros200@gmail.com>
2021-11-22 15:34:33 -0600

    se agrego la funcionalidad para que el cliente envia y reciba el video

--- Cliente.py
+++ Cliente.py
@@ -1,126 +1,37 @@
 import socket
-import threading
-from random import randint
+#import cv2 #Es la libreria de OpenCV. Se debe instalar con el comando "pip install opencv-contrib-python"
 
-servidor = ('localhost', 55555)
 
-print('')
-miUsuario = input('Escribe tu usuario: > ')
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(('localhost', 1002))
 
-# connect to servidor
-print('')
-print('=========== Conectando con el servidor ===========')
-print('')
+# #Aquí enviamos la imagen encriptada
+print('Enviando')
+file = open('./archivos/video.mp4', 'rb')
+video_data = file.read(2048)
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+while video_data:
+    client.send(video_data)
+    video_data = file.read(2048)
 
-direccion = '127.0.0.1' #Esta dirección es quivalente a poner localhost
-puerto = randint(1000, 5000)
+file.close()
+client.close()
+print('Exito')
 
-print('******** Tu dirección: "{}", '.format(direccion) + 'tu puerto: "{}" ********'.format(puerto))
 
-sock.bind((direccion, puerto))
+#------------------------------------------
+#SE RECIBE EL VIDEO CONVERTIDO EN BLANCO Y NEGRO
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(('localhost', 1002)) #192.168.1.252
 
-print('=========== Registrando con el servidor... ===========')
+print('Pidiendo video')
+file = open('./archivos/videoGris.mp4', 'wb')
+video_part = client.recv(2048)
+print('Recibiendo')
+while video_part:
+    file.write(video_part)
+    video_part = client.recv(2048)
 
-sock.sendto(miUsuario.encode(), servidor)
-
-while True:
-    data = sock.recv(128).decode()
-
-    if data.strip() == 'Hecho':
-        print('')
-        print('========= Registro correcto, esperando datos del otro Cliente... =========')
-        print('')
-        break
-
-data = sock.recv(1024).decode()
-dip, dport, = data.split(' ') #S recibe la informacion del otro cliente
-dport = int(dport) #Recibimos el puerto del otro cliente
-
-print('Datos recibidos con éxito!')
-print('')
-print('Dirección de Destino: "{}"'.format(dip))
-print('Puerto de Destino: "{}"'.format(dport))
-print('')
-
-print('=========== Conectando... ===========')
-print('')
-
-sock.sendto(miUsuario.encode(), (dip, dport)) #Enviamos el usuario para establecer la conexión
-
-suUsuario = sock.recv(128).decode()   #Recibimos el usuario del otro equipo para establecer la conexión
-
-print('=========== Listo para intercambiar mensajes ===========')
-print('')
-print('Tip: Escribe "exit" para salir. Sin comillas')
-print('Tip: Escribe "NombreArchivo.extensión/envArch" para enviar un archivo. Sin comillas')
-print('ASEGURATE DE QUE ANTES SE ENCUENTRE EN LA CARPETA DE "ARCHIVOS"')
-print('')
-
-detener_hilos = False   #boleano para matar los hilos inicializado en falso
-
-def escuchar():
-    while True:
-        data = sock.recv(2048).decode()  #Recibimos el mensaje
-
-        global detener_hilos    #Leemos la variable global del boleano para matar los hilos
-
-        if(data == 'exit'):    #Si el mensaje recibido dice "exit"...
-            print('')
-            print('')
-            print('== El otro Cliente ha cerrado la conexión. Presiona "Enter" para salir ==')
-            detener_hilos = True    #Cambiamos el valor a True para matar los hilos
-
-        if(detener_hilos):  #Si el valor es True...
-            break   #Matamos este hilo
-        
-        if '/envArch' in data:
-
-            nombreArchivo = data.split('/') #Leemos el mensaje y lo partimos en donde está la "/" para separar el nombre y extensión del comando en sí
-
-            file = open('./archivosR/' + nombreArchivo[0], 'wb')    #la posicion 0 de nombreArchivo contiene el nombre y extensión
-            file_part = sock.recv(1024)
-            print("\nRecibiendo archivo")
-            while file_part:
-                file.write(file_part)
-                file_part = sock.recv(1024)
-            file.close()
-            print("Archivo Recibido")
-            print(miUsuario + ': > ')
-        else: 
-            print('\r' + suUsuario + ': {}\n'.format(data) + miUsuario + ': > ', end='')    #Si no dice exit, ni el bool es True, entonces mostramos el mensaje
-       
-def decir():
-    while True:
-        msg = input(miUsuario + ': > ')   #Leemos la consola
-        
-        sock.sendto(msg.encode(), (dip, dport)) #Mandamos el mensaje al otro cliente
-        
-        if '/envArch' in msg:
-
-            nombreArchivo = msg.split('/')  #Leemos el mensaje y lo partimos en donde está la "/" para separar el nombre y extensión del comando en sí
-
-            file = open('./archivos/' + nombreArchivo[0], 'rb') #la posicion 0 de nombreArchivo contiene el nombre y extensión
-            file_data = file.read(1024)
-            while file_data:                           #Se podría implementar mejor con un Do-While y así se evita enviar el mensaje vacío
-                sock.sendto(file_data, (dip, dport))
-                file_data = file.read(1024)
-            sock.sendto(''.encode(), (dip, dport))  #Envia un mensaje vacío para salir del ciclo de recibir
-            file.close()
-
-        global detener_hilos    #Leemos la variable global del boleano para matar los hilos
-
-        if(msg == 'exit'):  #Si el mensaje que acabamos de mandar decía "exit..."
-            print('')
-            print('=========== Saliendo... ===========')
-            detener_hilos = True    #Cambiamos el valor a True para matar los hilos
-
-        if(detener_hilos):  #Si el valor es True...
-            break   ##Matamos este hilo
-
-escuchar = threading.Thread(target=escuchar, daemon=True)   #creamos el hilo para la función escuchar
-decir = threading.Thread(target=decir)  #creamos el hilo para la función decir
-
-escuchar.start()    #Iniciamos el hilo escuchar
-decir.start()   #Iniciamos el hilo decir
+file.close()
+client.close()
+print('Exito')
---
+++ ServidorMaestro.py
@@ -0,0 +1,79 @@
+import socket
+import cv2
+import numpy as np
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(('localhost', 1002)) #192.168.1.252
+server.listen()
+
+print('Esperando')
+
+client_socket, client_address = server.accept()
+#---------------------------------------------
+#EMPIEZA A RECIBIR EL VIDEO
+file = open('./archivos/video_cop.mp4', 'wb')
+video_part = client_socket.recv(2048)
+print('Recibiendo')
+while video_part:
+    file.write(video_part)
+    video_part = client_socket.recv(2048)
+
+file.close()
+client_socket.close()
+print('Exito')
+
+#Funcion del video
+video_path = './archivos/video_cop.mp4'
+frame_path = './Frames/Frame_'
+frame_gris = './FramesModificados/Frame_'
+cap = cv2.VideoCapture(video_path)
+
+
+img_index = 0
+while (cap.isOpened()):
+    ret, frame = cap.read()
+    if ret == False:
+        break
+    cv2.imwrite(frame_path + str(img_index) + '.png', frame)
+    imagen = cv2.imread(frame_path+str(img_index)+'.png')
+    gris = cv2.cvtColor(imagen, cv2.COLOR_BGR2GRAY)
+    cv2.imwrite(frame_gris + str(img_index) + '.png', gris)
+    img_index += 1
+
+cap.release()
+cv2.destroyAllWindows()
+
+
+#--------------Hacer el video--------------
+img_array = []
+
+img_index = 0
+for x  in range(0,40):
+    path = './FramesModificados/Frame_' + str(img_index) + '.png'    
+    img = cv2.imread(path)
+    alto, ancho, channels= img.shape
+    img_array.append(img)
+    img_index += 1
+
+video = cv2.VideoWriter('./archivos/video2.mp4', cv2.VideoWriter_fourcc(*'mp4v'), 29, (ancho, alto))
+for i in range(0,len(img_array)):
+    video.write(img_array[i])
+    
+video.release()
+print('Fin')
+
+
+#---------------------------------------------
+#SE ENVIA el video de vuelta al cliente
+client_socket, client_address = server.accept()
+print('Enviando')
+file = open('./archivos/video2.mp4', 'rb')
+image_data = file.read(2048)
+
+while image_data:
+    client_socket.send(image_data)
+    image_data = file.read(2048)
+
+file.close()
+client_socket.close()
+
